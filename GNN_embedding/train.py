e13cdf331583b0a0b517bc647d22533b99decb65
Kendrick Shen <kshen6@stanford.edu>
2019-12-07 11:15:12 -0800

    cuda fixes

--- GNN_embedding/train.py
+++ GNN_embedding/train.py
@@ -6,53 +6,23 @@ import numpy as np
 import load_entrez
 import load_assoc
 import copy
+from neural_net import GNN
+import utils
 
-## Given a graph and training mask, obtain 5 folds for train/val splits
-def make_cross_val_sets(data, n=5):
-    
-    idx_0 = np.where(data.y.cpu()[data.train_mask]==0)[0]
-    np.random.seed(40)
-    np.random.shuffle(idx_0)
-    intervals_0 = np.linspace(0,len(idx_0),n+1).astype(int)
-    
-    idx_1 = np.where(data.y.cpu()[data.train_mask]==1)[0]
-    np.random.seed(50)
-    np.random.shuffle(idx_1)
-    intervals_1 = np.linspace(0,len(idx_1),n+1).astype(int)
-    
-    masks = []
-    prev = 0
-    for i in range(1,n+1):
-        s = np.array([False]*len(data.y))
-        s[idx_0[intervals_0[i-1]:intervals_0[i]]] = True
-        s[idx_1[intervals_1[i-1]:intervals_1[i]]] = True
-        masks.append(s)
-    
-    return masks
-
-def get_acc(model, loader, is_val, device = None):
+def get_acc(model, loader, is_val = False):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     correct = 0
     total = 0
-    if is_val:
-        model.eval()
-    for d in loader:
-        if (device is not None):
-            d = d.to(device)
+    model.eval()
+    for data in loader:
+        data = data.to(device)
         with torch.no_grad():
-            if is_val:
-                pred = model(d).max(dim=1)[1][d.val_mask]
-                label = d.y.cpu()[d.val_mask]
-            else:
-                pred = model(d).max(dim=1)[1][d.test_mask]
-                label = d.y.cpu()[d.test_mask]
-            print(np.unique(pred,return_counts=True)[1])
-        correct += pred.eq(label).sum().item()
-
-if is_val:
-    total += torch.sum(d.val_mask).item()
-        else:
-            total += torch.sum(d.test_mask).item()
-return correct/total
+            pred = model(data).max(dim=1)[1]
+            label = data.y
+            print(np.unique(pred, return_counts=True)[1])
+        correct += pred[data.test_mask].eq(label[data.test_mask]).sum().item()
+        total += torch.sum(data.test_mask).item()
+    return correct/total
 
 def get_weight(x_):
     a,b = np.unique(x_, return_counts=True)[1]
@@ -73,39 +43,38 @@ def sample_from_mask(mask, data, k):
                 mask[i] = False
     return mask
 
-def train(loader, weight, epochs = 50,device=None):
+def train(loader, weight = None, epochs = 50):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     model = GNN(3, 32, 2, 'GCNConv')
-    if (device != None):
-        model = model.to(device)
+    model = model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
     criterion = F.nll_loss
     val_acc = []
-model_save = copy.deepcopy(model.cpu())
+    model_save = copy.deepcopy(model.cpu())
 
-for epoch in range(epochs):
-    model.train()
-    for batch in loader:
-        batch = batch.to(device)
-        optimizer.zero_grad()
-        out = model(batch)
-        loss = criterion(out[batch.train_mask], batch.y[batch.train_mask], weight=weight)
-        loss.backward()
-        optimizer.step()
-        print('loss on epoch', epoch, 'is', loss.item())
-        
-        if epoch % 5 == 0:
-            val_acc.append(get_acc(model, loader, is_val = True, device = device))
-            print('Validation:', val_acc[-1])
-            if(val_acc[-1]==np.max(val_acc)):
-                model_save = copy.deepcopy(model.cpu())
-                best_acc = val_acc[-1]
+    for epoch in range(epochs):
+        model.train()
+        for batch in loader:
+            batch = batch.to(device)
+            optimizer.zero_grad()
+            out = model(batch)
+            loss = criterion(out[batch.train_mask], batch.y[batch.train_mask])
+            loss.backward()
+            optimizer.step()
+            print('loss on epoch', epoch, 'is', loss.item())
+            
+            if epoch % 5 == 0:
+                val_acc.append(get_acc(model, loader, is_val = True))
+                print('Validation:', val_acc[-1])
+                if(val_acc[-1] == np.max(val_acc)):
+                    model_save = copy.deepcopy(model.cpu())
+                    best_acc = val_acc[-1]
 
-return val_acc, model_save, best_acc
+    return val_acc, model_save, best_acc
 
 def trainer(num_folds = 5):
     X_file = 'https://github.com/yhr91/CS224W_project/blob/master/Data/ForAnalysis/X/TCGA_GTEX_GeneExpression.csv?raw=true'
     y_file = 'https://github.com/yhr91/CS224W_project/raw/master/Data/ForAnalysis/Y/NCG_cancergenes_list.txt'
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     edgelist_file = 'https://github.com/yhr91/CS224W_project/blob/master/Data/PP-Decagon_ppi.csv?raw=true'
     # y_file = '../dataset_collection/DG-AssocMiner_miner-disease-gene.tsv'
     # edgelist_file = '../dataset_collection/PP-Decagon_ppi.csv'
@@ -113,45 +82,28 @@ def trainer(num_folds = 5):
     y = load_entrez.get_y(X, y_file)
     edges = load_entrez.get_edges(X, edgelist_file)
     
-    X = torch.tensor(X.iloc[:,1:4].values, dtype=torch.float, device = device)
-    y = torch.tensor(y,dtype=torch.long, device = device)
-    edges = torch.tensor(edges.values, dtype=torch.long, device = device)
+    X = torch.tensor(X.iloc[:,1:4].values, dtype=torch.float)
+    y = torch.tensor(y, dtype=torch.long)
+    edges = torch.tensor(edges.values, dtype=torch.long)
     
     # Set up train and test sets:
-    data = utils.load_pyg(X, edges, y, device=device)
-    masks = make_cross_val_sets(data, num_folds)
+    data_generator = utils.load_pyg(X, edges, y, folds=num_folds)
     
     # 5-fold cross validation
     val_accs, models, accs = [], [], []
-    for val_idx in range(num_folds):
-        print('Fold number:', val_idx)
-        train_idx = np.delete(np.arange(num_folds),val_idx)
-        train_mask = [False] * len(data.y)
-        for i in range(len(train_idx)):
-            train_mask = np.logical_or(train_mask,masks[i])
-    
-        ## For the validation mask, use the validation fold but
-        ## pick equal nodes from each class
-        val_mask = masks[val_idx]
-        val_mask = sample_from_mask(val_mask, data, 100)
-        
-        # Call training function
-        data.train_mask = torch.tensor(train_mask, dtype=torch.bool, device = device)
-        data.val_mask = torch.tensor(val_mask, dtype=torch.bool, device = device)
-        loader = DataLoader([data], batch_size=32, shuffle=True)
-        weight = get_weight(data.y.cpu()[train_mask])
-        
-        v, m, acc = train(loader, weight, device = device)
-        val_accs.append(v)
-        models.append(m)
-    accs.append(acc)
+    for idx, loader in enumerate(data_generator):
+        print('fold number:', idx)
+        val_acc, model, best_acc = train(loader)
+        val_accs.append(val_acc)
+        models.append(model)
+        accs.append(best_acc)
 
-best_model = models[np.argmax(accs)]
-print('Best model accuracy:')
-acc = get_acc(m, loader, is_val = False, device = device)
-print(acc)
+    best_model = models[np.argmax(accs)]
+    print('Best model accuracy:')
+    acc = get_acc(model, test_set, is_val = False)
+    print(acc)
 
-return val_accs
+    return val_accs
 
 if __name__ == '__main__':
     trainer()
--- GNN_embedding/utils.py
+++ GNN_embedding/utils.py
@@ -10,25 +10,59 @@ util functions
 
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import KFold
 
 import torch
-from torch_geometric.data import Data
+from torch_geometric.data import Data, DataLoader
 
-def load_pyg(X, edges, y, size=100, device = None):
-  # Create masks
-  np.random.seed(20) # to make sure the test set is always the same
-  ones = np.random.choice(np.where(y.cpu() == 1)[0], size=size, replace=False)
-  np.random.seed(30)
-  zeros = np.random.choice(np.where(y.cpu() == 0)[0], size=size, replace=False)
+def load_pyg(X, edges, y, folds=5):
+    kf = KFold(n_splits=folds, random_state=2)
+    for train_idx, test_idx in kf.split(X):
+        data = Data(x=X, edge_index=edges.t().contiguous(), y=y)
+        train_mask = [int(i in train_idx) for i in range(len(X))]
+        test_mask = [int(i in test_idx) for i in range(len(y))]
+        data.train_mask = torch.tensor(train_mask, dtype=torch.bool)
+        data.test_mask = torch.tensor(test_mask, dtype=torch.bool)
+        loader = DataLoader([data], batch_size=32, shuffle=True)
+        yield loader
 
-  test_mask = [True if i in ones or i in zeros 
-              else False for i in range(len(y))]
-  train_mask = [True if i not in ones and i not in zeros 
-                else False for i in range(len(y))]
+    # # Create masks
+    # np.random.seed(20) # to make sure the test set is always the same
+    # ones = np.random.choice(np.where(y.cpu() == 1)[0], size=size, replace=False)
+    # np.random.seed(30)
+    # zeros = np.random.choice(np.where(y.cpu() == 0)[0], size=size, replace=False)
 
-  # Return data loader
-  data = Data(x=X, edge_index=edges.t().contiguous(), y=y)
-  data.train_mask = torch.tensor(train_mask, dtype=torch.bool, device = device)
-  data.test_mask = torch.tensor(test_mask, dtype=torch.bool, device = device)
+    # test_mask = [True if i in ones or i in zeros 
+    #             else False for i in range(len(y))]
+    # train_mask = [True if i not in ones and i not in zeros 
+    #             else False for i in range(len(y))]
 
-  return data
+    # # Return data loader
+    # data = Data(x=X, edge_index=edges.t().contiguous(), y=y)
+    # data.train_mask = torch.tensor(train_mask, dtype=torch.bool, device = device)
+    # data.test_mask = torch.tensor(test_mask, dtype=torch.bool, device = device)
+
+    # return data
+
+## Given a graph and training mask, obtain 5 folds for train/val splits
+def make_cross_val_sets(data, n=5):
+    
+    idx_0 = np.where(data.y.cpu()[data.train_mask]==0)[0]
+    np.random.seed(40)
+    np.random.shuffle(idx_0)
+    intervals_0 = np.linspace(0,len(idx_0),n+1).astype(int)
+    
+    idx_1 = np.where(data.y.cpu()[data.train_mask]==1)[0]
+    np.random.seed(50)
+    np.random.shuffle(idx_1)
+    intervals_1 = np.linspace(0,len(idx_1),n+1).astype(int)
+    
+    masks = []
+    prev = 0
+    for i in range(1,n+1):
+        s = np.array([False]*len(data.y))
+        s[idx_0[intervals_0[i-1]:intervals_0[i]]] = True
+        s[idx_1[intervals_1[i-1]:intervals_1[i]]] = True
+        masks.append(s)
+    
+    return masks
