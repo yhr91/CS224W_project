2c3c66843227400b95b538835102c14361942c22
yhr91 <yusuf.roohani@gmail.com>
2020-03-28 14:59:59 -0700

    tfboard working

--- GNN_embedding/train.py
+++ GNN_embedding/train.py
@@ -7,51 +7,51 @@ import torch
 import torch.nn.functional as F
 from datetime import datetime
 import numpy as np
-#from load_assoc_ben import ProcessData
 from load_assoc import ProcessData
 from neural_net import GNN
 import utils
 from torch.utils.tensorboard import SummaryWriter
 import copy
-from sklearn.metrics import f1_score
 
 
 def train(loader, args, epochs=100):
-    writer = SummaryWriter('tensorboard_runs/gcn')
+    writer = SummaryWriter('tensorboard_runs/gcn/'+args.network_type+'_'+args.dataset)
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     model = GNN(args.in_dim, args.hidden_dim, args.out_dim, args.network_type)
     model = model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
     criterion = F.nll_loss
-    val_f1 = []
-    losses = []
+    best_f1 = 0
     model_save = copy.deepcopy(model.cpu())
 
     for epoch in range(epochs):
         model.train()
+
         for batch in loader:
             batch = batch.to(device)
             optimizer.zero_grad()
             out = model(batch)
-            # writer.add_graph(model, batch)
             weight = utils.get_weight(batch.y, device=device)
             loss = criterion(out[batch.train_mask],
                              batch.y[batch.train_mask],weight=weight)
             loss.backward()
             optimizer.step()
-            losses.append(loss.item())
+            writer.add_scalar('Loss/train', loss.item(), epoch)
             print('loss on epoch', epoch, 'is', loss.item())
 
             if epoch % 1 == 0:
-                val_f1.append(utils.get_acc(model, loader, is_val=True)['f1'])
-                print('Validation:', val_f1[-1])
-                if (val_f1[-1] == np.max(val_f1)):
+                val_f1 = utils.get_acc(model, loader, is_val=True)['f1']
+                writer.add_scalar('F1/validation', val_f1, epoch)
+                print('Validation:', val_f1)
+
+                if val_f1 > best_f1:
                     model_save = copy.deepcopy(model.cpu())
-                    best_f1 = val_f1[-1]
+                    best_f1 = val_f1
+
     writer.flush()
     writer.close()
-    return val_f1, model_save, best_f1, losses
+    return model_save, best_f1
 
 
 def trainer(args, num_folds=5):
@@ -95,13 +95,11 @@ def trainer(args, num_folds=5):
         data_generator = utils.load_pyg(X, edges, y, folds=num_folds, test_size=test_size)
 
         # 5-fold cross validation
-        val = [] # val f1 scores
         models = [] # save models for now
         model_f1s = [] # save model recalls
 
         for loader in data_generator:
-            val_f1, model, best_f1, _ = train(loader, args)
-            val.append(val_f1)
+            model, best_f1 = train(loader, args)
             models.append(model)
             model_f1s.append(best_f1)
 
@@ -109,7 +107,7 @@ def trainer(args, num_folds=5):
         print('Best model accuracy:')
         test_recall = utils.get_acc(best_model, loader, is_val=False)
         print(test_recall)
-        curr_results[ind] = [val, test_recall]
+        curr_results[ind] = [test_recall]
 
     dt = str(datetime.now())[8:19].replace(' ', '-')
     curr_file = open(f'bensmodels/{dt}-{ind}-thru-{ind+100}.txt', 'w')
@@ -119,8 +117,8 @@ def trainer(args, num_folds=5):
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Define network type and dataset.')
-    parser.add_argument('network_type', type=str, choices=['GCNConv', 'SAGEConv', 'GATConv'])
-    parser.add_argument('dataset', type=str, choices=['Decagon', 'GNBR', 'Decagon_GNBR'])
+    parser.add_argument('--network_type', type=str, choices=['GCNConv', 'SAGEConv', 'GATConv'], default='GCNConv')
+    parser.add_argument('--dataset', type=str, choices=['Decagon', 'GNBR', 'Decagon_GNBR'], default='GNBR')
     parser.add_argument('--in_dim', type=int, default=11)
     parser.add_argument('--hidden_dim', type=int, default=32)
     parser.add_argument('--out_dim', type=int, default=2)
