77f43cc77b3ad3a812010902c1089bffd6e6db1c
yhr91 <yusuf.roohani@gmail.com>
2020-03-28 23:09:31 -0700

    corrected alignment

--- GNN_embedding/train.py
+++ GNN_embedding/train.py
@@ -17,44 +17,44 @@ def train(loader, args, ind, it, epochs=100):
     writer = SummaryWriter('./tensorboard_runs/gcn/'+args.expt_name+'/'
                            +args.network_type+'_'+args.dataset)
         
-                           device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-                           model = GNN(args.in_dim, args.hidden_dim, args.out_dim, args.network_type)
-                           model = model.to(device)
-                           optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
-                           criterion = F.nll_loss
-                           best_f1 = 0
-                           model_save = copy.deepcopy(model.cpu())
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    model = GNN(args.in_dim, args.hidden_dim, args.out_dim, args.network_type)
+    model = model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+    criterion = F.nll_loss
+    best_f1 = 0
+    model_save = copy.deepcopy(model.cpu())
                            
-                           for epoch in range(epochs):
-                               model.train()
-                               
-                               for batch in loader:
-                                   batch = batch.to(device)
-                                   optimizer.zero_grad()
-                                   out = model(batch)
-                                   weight = utils.get_weight(batch.y, device=device)
-                                   loss = criterion(out[batch.train_mask],
-                                                    batch.y[batch.train_mask],weight=weight)
-                                                    loss.backward()
-                                                        optimizer.step()
-                                                        writer.add_scalar('TrainLoss/disease_'+str(ind), loss.item(), it*epochs+epoch)
-                                                        print('loss on epoch', epoch, 'is', loss.item())
-                                                        
-                                                        if epoch % 1 == 0:
-                                                            val_f1 = utils.get_acc(model, loader, is_val=True)['f1']
-                                                            writer.add_scalar('ValF1/disease_'+str(ind), val_f1, it*epochs+epoch)
-                                                            
-                                                            val_recall = utils.get_acc(model, loader, is_val=True)['recall']
-                                                            writer.add_scalar('ValRecall/disease_' + str(ind), val_f1, it*epochs+epoch)
-                                                            print('Validation:', val_f1)
-                                                            
-                                                            if val_f1 > best_f1:
-                                                                model_save = copy.deepcopy(model.cpu())
-                                                                best_f1 = val_f1
+    for epoch in range(epochs):
+        model.train()
 
-writer.flush()
-writer.close()
-return model_save, best_f1
+    for batch in loader:
+        batch = batch.to(device)
+        optimizer.zero_grad()
+        out = model(batch)
+        weight = utils.get_weight(batch.y, device=device)
+        loss = criterion(out[batch.train_mask],
+        batch.y[batch.train_mask],weight=weight)
+        loss.backward()
+        optimizer.step()
+        writer.add_scalar('TrainLoss/disease_'+str(ind), loss.item(), it*epochs+epoch)
+        print('loss on epoch', epoch, 'is', loss.item())
+
+        if epoch % 1 == 0:
+            val_f1 = utils.get_acc(model, loader, is_val=True)['f1']
+            writer.add_scalar('ValF1/disease_'+str(ind), val_f1, it*epochs+epoch)
+
+            val_recall = utils.get_acc(model, loader, is_val=True)['recall']
+            writer.add_scalar('ValRecall/disease_' + str(ind), val_f1, it*epochs+epoch)
+            print('Validation:', val_f1)
+
+            if val_f1 > best_f1:
+                model_save = copy.deepcopy(model.cpu())
+                best_f1 = val_f1
+
+    writer.flush()
+    writer.close()
+    return model_save, best_f1
 
 
 def trainer(args, num_folds=5):
@@ -63,14 +63,14 @@ def trainer(args, num_folds=5):
         'GNBR': '../dataset_collection/GNBR-edgelist.csv',
         'Decagon_GNBR': '../dataset_collection/Decagon_GNBR.csv'
     }[args.dataset]
-    
+
     processed_data = ProcessData(edgelist_file, use_features=args.use_features)
     X = processed_data.X
     X = torch.tensor(X.values, dtype=torch.float)
     disease_test_scores = {}
 
-# Iterate over diseases
-dir_ = './tensorboard_runs/gcn/'+args.expt_name
+    # Iterate over diseases
+    dir_ = './tensorboard_runs/gcn/'+args.expt_name
     
     for ind, column in enumerate(processed_data.Y):
         if ind > 100: break # TODO: Remove this later on. For testing purposes only
@@ -86,23 +86,23 @@ dir_ = './tensorboard_runs/gcn/'+args.expt_name
         data_generator = utils.load_pyg(X, edges, y,
                                         folds=num_folds, test_size=test_size)
             
-                                        # 5-fold cross validation
-                                        models = [] # save models for now
-                                        model_scores = [] # save model recalls
-                                        
-                                        for it,loader in enumerate(data_generator):
-                                            model, score = train(loader, args, ind, it)
-                                            models.append(model)
-                                            model_scores.append(score)
-                                        
-                                        best_model = models[np.argmax(model_scores)]
-                                            best_test_score = utils.get_acc(best_model, loader, is_val=False)
-                                            print('Best model f1:')
-print(best_test_score)
-disease_test_scores[ind] = [best_test_score]
+        # 5-fold cross validation
+        models = [] # save models for now
+        model_scores = [] # save model recalls
 
-# Save results
-    np.save(dir_+'/results', disease_test_scores)
+        for it,loader in enumerate(data_generator):
+            model, score = train(loader, args, ind, it)
+            models.append(model)
+            model_scores.append(score)
+
+        best_model = models[np.argmax(model_scores)]
+        best_test_score = utils.get_acc(best_model, loader, is_val=False)
+        print('Best model f1:')
+        print(best_test_score)
+        disease_test_scores[ind] = [best_test_score]
+
+        # Save results
+        np.save(dir_+'/results', disease_test_scores)
 
 if __name__ == '__main__':
     import argparse
@@ -131,5 +131,5 @@ if __name__ == '__main__':
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
 
-seed_torch()
-trainer(args)
+    seed_torch()
+    trainer(args)
