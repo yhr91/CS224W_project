30735a72a90dd0f9cb1c5e0b52eb5e85a575215d
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-04-19 05:31:40 +0000

    minor changes

--- GNN_embedding/train.py
+++ GNN_embedding/train.py
@@ -234,7 +234,7 @@ if __name__ == '__main__':
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--shuffle', type=bool, nargs ='?', const=True, default=False)
     parser.add_argument('--score', type=str, default='f1_sum')
-    parser.add_argument('--sample-diseases', type=bool, nargs='?', const=True, default=True)
+    parser.add_argument('--sample-diseases', type=bool, nargs='?', const=True, default=False)
     parser.add_argument('--disease_class', type=str, default=False)
     # parser.add_argument('--heterogeneous', type=bool, nargs='?', const=True, default=False)
     args = parser.parse_args()
